bbad0e58b2a3c2a4f478557e81fe0d4ca65fd872
Nicholas Davis <nickdavis1018@gmail.com>
2021-09-27 22:43:53 -0700

    added eupdate and create routes

--- server.js
+++ server.js
@@ -26,6 +26,27 @@ app.get('/pokemon/new', (req, res) => {
     console.log("You're a Poke-God! Create at will - the world is your Cloyster.")
   });
 
+app.put("/pokemon/:id", (req, res) => {
+    pokemon[req.params.id] = req.body
+    res.redirect("/pokemon")
+    console.log(`Put your update for ${pokemon[req.params.id].name} in the Pokedex!`)
+    });
+
+app.post("/pokemon", (req, res) => {
+    req.body.img = req.body.img + ".png"
+    let idVerify = pokemon.slice(-1)
+    req.body.id = idVerify.id + 1
+    pokemon.push(req.body)
+    res.redirect("/pokemon")
+    console.log(`Posted ${req.body.name} to the Pokedex with an ID of ${req.body.id}!`)
+    });
+
+app.get('/pokemon/:id/edit', (req, res) => {
+    res.render("edit.ejs", {pokemonEdit: pokemon[req.params.id]})
+    let editKey = req.params.id
+    console.log(`Edit info and attributes for ${pokemon[req.params.id].name}. Changes will be saved to the Pokedex.`)
+  });
+
 app.get('/pokemon/:id', (req, res) => {
     res.render("show.ejs", {pokemonShow: pokemon[req.params.id]})
     console.log(`Welcome to the Pokedex's ${pokemon[req.params.id].name} Info Page.`)
